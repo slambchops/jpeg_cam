08f22b6160f2b2de1c9440bacc4af79d34c1ee13
Josh Elliott <jelliott.uf@gmail.com>
2014-09-26 03:12:46 +0000

    Allow picture file names to be passed to the program via the command line.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include "camera.h"
 
 int main(int argc, char *argv[])
 {
-	char out[] = "camera.jpg";
+	char out[32];	
+
+	if (argv[1] == '\0')
+		strcpy(out, "image.jpg");
+	else
+		strcpy(out, argv[1]);
 
 	init_camera();
 
